c1da2e2590d77b88148d3f08e23336e3cca664d5
HidetoNakasone <highenjoyproject@outlook.jp>
2018-11-06 02:44:55 +0900

    go git

---
+++ app.rb
@@ -0,0 +1,28 @@
+
+require "sinatra"
+require "sinatra/reloader"
+require "zip_code_jp"
+
+enable :sessions
+
+def seisei(postNum)
+  address = ZipCodeJp.find postNum.to_s
+  return res = {"prefecture":address.prefecture, "prefecture_kana":address.prefecture_kana, "prefecture_code":address.prefecture_code, "city":address.city, "city_kana":address.city_kana, "town":address.town, "town_kana":address.town_kana, "zip_code":address.zip_code}
+end
+
+get '/' do
+  if session[:res].nil?
+    redirect '/form'
+  else
+    erb :view, :layout => :layout
+  end
+end
+
+get '/form' do
+  erb :form, :layout => :layout
+end
+
+post '/form' do
+  session[:res]  = seisei(params[:postNum])
+  redirect '/'
+end
