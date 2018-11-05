920487ba5a39bbb901cb8d3ce3ac58451bcc92bf
HidetoNakasone <highenjoyproject@outlook.jp>
2018-11-06 03:41:57 +0900

    re

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require "zip_code_jp"
 
 enable :sessions
 
-def seisei(postNum)
-  address = ZipCodeJp.find postNum.to_s
+def seisei(postNumA, postNumB)
+  address = ZipCodeJp.find "#{postNumA}-#{postNumB}"
   return res = {"prefecture":address.prefecture, "prefecture_kana":address.prefecture_kana, "prefecture_code":address.prefecture_code, "city":address.city, "city_kana":address.city_kana, "town":address.town, "town_kana":address.town_kana, "zip_code":address.zip_code}
 end
 
@@ -23,6 +23,6 @@ get '/form' do
 end
 
 post '/form' do
-  session[:res]  = seisei(params[:postNum])
+  session[:res]  = seisei(params[:postNumA], params[:postNumB])
   redirect '/'
 end
